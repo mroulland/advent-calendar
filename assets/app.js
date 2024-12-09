153f1425d7e4ae7b830d2b113d93e3dc21d46932
mroulland <morgane.roulland@hotmail.fr>
2024-12-09 21:33:30 +0100

    Fix display users pictures

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,18 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const snowflakes = document.querySelectorAll(".snowflake");
+  
+    snowflakes.forEach(snowflake => {
+      // Position horizontale aléatoire
+      const randomX = Math.floor(Math.random() * 100); // Entre 0% et 100%
+      const randomDuration = Math.random() * (20 - 5) + 5; // Entre 3s et 5s pour la durée
+  
+      // Appliquer ces valeurs via JavaScript
+      snowflake.style.left = `${randomX}%`; // Appliquer position X aléatoire
+      snowflake.style.animationDuration = `${randomDuration}s`; // Appliquer durée d'animation aléatoire
+    });
+  });
+  
